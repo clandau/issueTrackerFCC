8272f4390ba8b93cd1629c4a2927bb1569721214
clandau <so.super@gmail.com>
2019-01-11 15:11:32 -0500

    work on post function schema

--- routes/api.js
+++ routes/api.js
@@ -25,22 +25,31 @@ module.exports = function (app) {
 
   app.route('/api/issues/:project')
     .get(function (req, res){
-      let project = req.params.project;
-      
+      let project = req.params.project
+      //returns a JSON array of all issues on current project with all info for each issue
     })
     
     .post(function (req, res){
-      let project = req.params.project;
-      
+      let project = req.params.project
+      let now = new Date()
+      let issueObj = {
+        issue_title : req.body.issue_title,
+        issue_text : req.body.issue_text,
+        created_by : req.body.created_by,
+        created_on : now,
+        updated_on : now,
+        open : true
+      }
+      console.log(issueObj)
+      res.send(issueObj)
     })
     
     .put(function (req, res){
-      let project = req.params.project;
-      
+      let project = req.params.project
     })
     
     .delete(function (req, res){
-      let project = req.params.project;
+      let project = req.params.project
       
     })
     
